4fe6d417c371569e2c091b6404b296490723b5c0
tae98 <98rlaxogus@gmail.com>
2024-08-30 17:17:53 +0900

    Add tests and update entities for unidirectional One-to-Many relationship

--- src/main/java/com/sparta/jpaadvance/Entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/Entity/Food.java
@@ -1,8 +1,11 @@
 package com.sparta.jpaadvance.Entity;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Getter
@@ -15,9 +18,7 @@ public class Food {
     private String name;
     private double price;
 
-    @ManyToOne
-    //FK column 명은 name
-    //관계를 맺고 싶은 entity 를 필드로 가짐
-    @JoinColumn(name = "user_id")
-    private User user;
+    @OneToMany
+    @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    private List<User> userList = new ArrayList<>();
 }
