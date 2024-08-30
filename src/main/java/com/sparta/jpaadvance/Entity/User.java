20c1971be93a358bdc71f5ef843539ec848d636c
tae98 <98rlaxogus@gmail.com>
2024-08-30 17:37:45 +0900

    Add tests and update entities for bidirectional Many-to-Many relationship

--- src/main/java/com/sparta/jpaadvance/Entity/User.java
+++ src/main/java/com/sparta/jpaadvance/Entity/User.java
@@ -1,8 +1,12 @@
 package com.sparta.jpaadvance.Entity;
 
+import com.sparta.jpaadvance.Entity.Food;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,4 +17,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "userList")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.getUserList().add(this); // 외래 키(연관 관계) 설정
+    }
 }
